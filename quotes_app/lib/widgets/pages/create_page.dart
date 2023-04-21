83b250f9184bcae0a719efbe73a38fd5f3a97c9e
emilieiios <89915196+emilieiios@users.noreply.github.com>
2023-04-21 21:20:35 +0200

    create bottomnavigationbar and pages

---
+++ quotes_app/lib/widgets/pages/create_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CreatePage extends StatelessWidget {
+  const CreatePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Create');
+  }
+
+}
